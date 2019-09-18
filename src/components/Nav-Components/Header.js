f5a1a2f0ce1df1e6f067aeeca4ce79806ef36a58
unknown <38829219+deepklim@users.noreply.github.com>
2019-09-18 11:27:41 -0400

    Re-style navbar with new School logos

--- src/components/Nav-Components/Header.js
+++ src/components/Nav-Components/Header.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
 import GoC_EN from '../../static/FIPs/goc_colour_en.svg';
 import styles from './Header.css';
 
 function Nav() {
 	return (
-		<header className={styles.header}>
-			<a href="https://www.canada.ca/en.html">
-				<img src={GoC_EN} alt="Government of Canada Logo" />
-			</a>
-			<a className={styles.langButtonDesktop} href="https://www.google.ca">Français</a>
-		</header>
+		<Container>
+			<header className={styles.header}>
+				<a href="https://www.canada.ca/en.html">
+					<img src={GoC_EN} alt="Government of Canada Logo" />
+				</a>
+				<a className={styles.langButtonDesktop} href="https://www.google.ca">Français</a>
+			</header>
+		</Container>
 	);
 }
 
