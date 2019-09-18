0dc5f0ba24a1c13c96bed332a6d845b463d64c29
unknown <38829219+deepklim@users.noreply.github.com>
2019-09-18 11:27:00 -0400

    Add new navbar

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import React from 'react';
 import './static/App.css';
 import Container from '@material-ui/core/Container';
 import Header from './components/Nav-Components/Header';
+import Nav from './components/Nav-Components/Nav';
+
+
+
 import ApiKey from './components/Comment-Components/ApiKey';
 import Comments from './components/Comment-Components/Comments';
 import Counts from './components/Comment-Components/Counts';
+
+
 import Footer from './components/Nav-Components/Footer';
 
 function App() {
 	return (
-		<Container>
+		<>
 			<Header />
-			<ApiKey />
-			<Counts />
-			<Comments />
+			<Nav />
+			
+			<Container>
+				<ApiKey />
+				<Counts />
+				<Comments />
+			</Container>
+			
 			<Footer />
-		</Container>
+		</>
 	);
 }
 
