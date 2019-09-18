e7bb4b914f14e4e7be1e14a95fcd354b69a41889
unknown <38829219+deepklim@users.noreply.github.com>
2019-09-18 11:26:38 -0400

    Initial upload

---
+++ src/components/Nav-Components/Nav.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import CSPS from '../../static/FIPs/csps_logo.svg';
+import styles from './Nav.css';
+
+function Nav() {
+	return (
+		<div className={styles.outerBanner}>
+			<Container>
+				<div className={styles.banner}>
+					<div className={styles.navBar}>
+						<ul>
+							<li><a href="#">Calendar</a></li>
+							<li><a href="#">About</a></li>
+							<li><a href="#">Home</a></li>
+						</ul>
+					</div>
+					
+					<div className={styles.logos}>
+						<img src={CSPS} alt="CSPS Logo" />
+						<h1>CSPS Data Explorer</h1>
+					</div>
+				</div>
+			</Container>
+		</div>
+	);
+}
+
+export default Nav;
