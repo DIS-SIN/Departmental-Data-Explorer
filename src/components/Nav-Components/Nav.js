26acf306015f83cf171321c2c80c007e15c9f50a
unknown <38829219+deepklim@users.noreply.github.com>
2019-09-18 16:02:25 -0400

    Update CSS for hover effect in navbar

--- src/components/Nav-Components/Nav.js
+++ src/components/Nav-Components/Nav.js
@@ -4,6 +4,17 @@ import MobileDrawer from './MobileDrawer';
 import CSPS from '../../static/FIPs/csps_logo.svg';
 import styles from './Nav.css';
 
+function NavLink(props) {
+	return (
+		<a
+			className={props.active ? styles.active : ''}
+			href={props.href}
+			>
+			{props.label}
+		</a>
+	);
+}
+
 function Nav() {
 	return (
 		<div className={styles.outerBanner}>
@@ -15,9 +26,9 @@ function Nav() {
 				<div className={styles.banner}>
 					<div className={styles.navBar}>
 						<ul>
-							<li><a href="#">Calendar</a></li>
-							<li><a href="#">About</a></li>
-							<li><a href="#">Home</a></li>
+							<li><NavLink active={true} href="#" label="Calendar" /></li>
+							<li><NavLink active={false} href="#" label="About" /></li>
+							<li><NavLink active={false} href="#" label="Home" /></li>
 						</ul>
 					</div>
 					
