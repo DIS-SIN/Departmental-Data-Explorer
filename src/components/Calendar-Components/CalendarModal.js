6f7cf9f8b3f9f8dc0ffc31b9b4808013c59e7d9d
unknown <38829219+deepklim@users.noreply.github.com>
2019-09-26 17:04:19 -0400

    Switch to RGBA as 8-digit hex not supported in IE11

--- src/components/Calendar-Components/CalendarModal.js
+++ src/components/Calendar-Components/CalendarModal.js
@@ -37,7 +37,7 @@ class CalendarModal extends Component {
 					isOpen={this.state.modalOpen}
 					onRequestClose={this.toggleModal}
 					className={styles.modal}
-					style={{ overlay: { backgroundColor: '#00000080' } }}
+					style={{ overlay: { backgroundColor: 'rgba(0, 0, 0, 0.5)' } }}
 				>
 					<div className="modal-header" onClick={this.toggleModal}>
 						<button className="close">&times;</button>
