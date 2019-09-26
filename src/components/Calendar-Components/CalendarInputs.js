f2ef967761eb9833b099c357ded2f1809463c25c
unknown <38829219+deepklim@users.noreply.github.com>
2019-09-26 15:31:13 -0400

    Add state; pass callback to children to set from modal

--- src/components/Calendar-Components/CalendarInputs.js
+++ src/components/Calendar-Components/CalendarInputs.js
@@ -4,12 +4,28 @@ import DatePicker from './DatePicker';
 import styles from './CalendarInputs.css';
 
 class CalendarInputs extends Component {
+	state = {
+		modalBusinessLine: '',
+		modalClientsOnly: false,
+		modalCourseCode: '',
+		modalExcludeCancelled: false,
+		modalInstructor: ''
+	};
+	
+	changeInput = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	}
+	
+	clearInput = (e) => {
+		this.setState({ [e.target.attributes.name.value]: '' });
+	}
+	
 	render() {
 		return (
 			<div className={styles.calendarInputs}>
-				<DatePicker label="From:"/>
-				<CalendarModal />
-				<DatePicker label="To:"/>
+				<DatePicker label="From:" />
+				<CalendarModal changeInput={this.changeInput} clearInput={this.clearInput} currentCalendarOptions={this.state} />
+				<DatePicker label="To:" />
 			</div>
 		);
 	}
--- src/components/Calendar-Components/calendarModal.js
+++ src/components/Calendar-Components/calendarModal.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import Modal from 'react-modal';
 import styles from './CalendarModal.css';
 
+function InputBox(props) {
+	return (
+		<>
+			<td><input type="text" name={props.name} value={props.currentCalendarOptions[props.name]} onChange={props.changeInput} /></td>
+			<td className={styles.clearInputBox} name={props.name} onClick={props.clearInput}>&times;</td>
+		</>
+	);
+}
+
 class CalendarModal extends Component {
-	state = {
-		modalOpen: false
-	};
+	constructor(props) {
+		super(props);
+		this.state = {
+			modalOpen: false
+		};
+	}
 	
 	toggleModal = () => {
 		this.setState({ modalOpen: !this.state.modalOpen });
@@ -37,7 +49,7 @@ class CalendarModal extends Component {
 									<tr>
 										<td>Business Line</td>
 										<td>
-											<select id="modal-business-line">
+											<select name="modalBusinessLine" onChange={this.props.changeInput}>
 												<option value="">All</option>
 												<option value="Digital Academy">Digital Academy</option>
 												<option value="GC and Public Sector Skills">GC and Public Sector Skills</option>
@@ -50,18 +62,26 @@ class CalendarModal extends Component {
 									</tr>
 									<tr>
 										<td>Course Code</td>
-										<td><input type="text" value="" /></td>
-										<td className={styles.clearSiblingInput}>&times;</td>
+										<InputBox
+											name="modalCourseCode"
+											changeInput={this.props.changeInput}
+											clearInput={this.props.clearInput}
+											currentCalendarOptions={this.props.currentCalendarOptions}
+										/>
 									</tr>
 									<tr>
 										<td>Instructor</td>
-										<td><input type="text" value="" /></td>
-										<td className={styles.clearSiblingInput}>&times;</td>
+										<InputBox
+											name="modalInstructor"
+											changeInput={this.props.changeInput}
+											clearInput={this.props.clearInput}
+											currentCalendarOptions={this.props.currentCalendarOptions}
+										/>
 									</tr>
 									<tr>
 										<td>Exclude Cancelled Offerings</td>
 										<td>
-											<select>
+											<select name="modalExcludeCancelled" onChange={this.props.changeInput}>
 												<option value="false">No</option>
 												<option value="true">Yes</option>
 											</select>
@@ -71,7 +91,7 @@ class CalendarModal extends Component {
 									<tr>
 										<td>Show Only Client Requests</td>
 										<td>
-											<select>
+											<select name="modalClientsOnly" onChange={this.props.changeInput}>
 												<option value="false">No</option>
 												<option value="true">Yes</option>
 											</select>
