32aba12720f502fa94fa5a58b55c5d5a05f19156
unknown <38829219+deepklim@users.noreply.github.com>
2019-09-28 00:10:10 -0400

    Add AJAX call to Registhor for today's offerings

--- src/components/Calendar-Components/Calendar.js
+++ src/components/Calendar-Components/Calendar.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CalendarInputs from './CalendarInputs';
+import { REGISTHOR_API_KEY } from '../../utils/API_KEYS';
+import Map from './Map';
 import styles from './Calendar.css';
 
-function Calendar() {
-	return (
-		<>
-			<h3 className={styles.h3}>Search for offerings by date, instructor, and more.</h3>
-			<CalendarInputs />
-		</>
-	);
+class Calendar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			cityCounts: []
+		}
+	}
+	
+	// Make formatting date a func
+	
+	componentDidMount() {
+		// Registhor accepts dates in standard YYYY-MM-DD ISO format.
+		let today = new Date(Date.now());
+		let todayYear = today.getFullYear();
+		// Months are 0-indexed in JS
+		let todayMonth = today.getMonth() + 1;
+		let todayDay = today.getDate();
+		let isoDate = `${todayYear}-${todayMonth}-${todayDay}`;
+		fetch(`https://registhor.da-an.ca/api/v1/offering-counts?key=${REGISTHOR_API_KEY}&date_1=${isoDate}&exclude_cancelled=false`)
+			.then(resp => resp.json())
+			.then((data) => {
+				this.setState({
+					cityCounts: data.results
+				})
+			});
+	}
+	
+	render() {
+		return (
+			<>
+				<h3 className={styles.h3}>Search for offerings by date, instructor, and more.</h3>
+				<CalendarInputs />
+				<Map cityCounts={this.state.cityCounts} />
+			</>
+		);
+	}
 }
 
 export default Calendar;
