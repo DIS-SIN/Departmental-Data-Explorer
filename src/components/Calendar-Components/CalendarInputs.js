d0d6883ce0557bbc7d31f89df9388812c442db25
unknown <38829219+deepklim@users.noreply.github.com>
2019-09-30 16:09:22 -0400

    Refactor as a functional component

--- src/components/Calendar-Components/CalendarInputs.js
+++ src/components/Calendar-Components/CalendarInputs.js
@@ -3,27 +3,25 @@ import CalendarModal from './CalendarModal';
 import DatePicker from './DatePicker';
 import styles from './CalendarInputs.css';
 
-class CalendarInputs extends Component {
-	render() {
-		return (
-			<div className={styles.calendarInputs}>
-				<DatePicker
-					label="From:"
-					changeInputs={this.props.changeInputs}
-					currentInputs={this.props.currentInputs}
-				/>
-				<CalendarModal
-					changeInputs={this.props.changeInputs}
-					currentInputs={this.props.currentInputs}
-				/>
-				<DatePicker
-					label="To:"
-					changeInputs={this.props.changeInputs}
-					currentInputs={this.props.currentInputs}
-				/>
-			</div>
-		);
-	}
+function Component() {
+	return (
+		<div className={styles.calendarInputs}>
+			<DatePicker
+				label="From:"
+				changeInputs={this.props.changeInputs}
+				currentInputs={this.props.currentInputs}
+			/>
+			<CalendarModal
+				changeInputs={this.props.changeInputs}
+				currentInputs={this.props.currentInputs}
+			/>
+			<DatePicker
+				label="To:"
+				changeInputs={this.props.changeInputs}
+				currentInputs={this.props.currentInputs}
+			/>
+		</div>
+	);
 }
 
 export default CalendarInputs;
