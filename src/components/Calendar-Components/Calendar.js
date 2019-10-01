3a6df5a24c432aa2343403ccae21020745610465
unknown <38829219+deepklim@users.noreply.github.com>
2019-10-01 11:40:39 -0400

    Store user inputs in separate object

--- src/components/Calendar-Components/Calendar.js
+++ src/components/Calendar-Components/Calendar.js
@@ -9,29 +9,33 @@ class Calendar extends Component {
 		super(props);
 		this.state = {
 			cityCounts: [],
-			datePickerFrom: new Date(),
-			datePickerTo: new Date(),
-			modalBusinessLine: '',
-			modalClientsOnly: false,
-			modalCourseCode: '',
-			modalExcludeCancelled: false,
-			modalInstructor: ''
+			inputValues: {
+				datePickerFrom: new Date(),
+				datePickerTo: new Date(),
+				modalBusinessLine: '',
+				modalClientsOnly: false,
+				modalCourseCode: '',
+				modalExcludeCancelled: false,
+				modalInstructor: ''
+			}
 		}
 	}
 	
 	changeInputs = (newInputs) => {
-		this.setState({ ...newInputs }, () => {
+		this.setState((state, props) => {
+			return { inputValues: {...state.inputValues, ...newInputs} };
+		}, () => {
 			this.getCountsRegisthor();
 		});
 	}
 	
 	getISO = (myDate) => {
 		// Registhor accepts dates in standard YYYY-MM-DD ISO format.
-		let todayYear = myDate.getFullYear();
+		let year = myDate.getFullYear();
 		// Months are 0-indexed in JS
-		let todayMonth = myDate.getMonth() + 1;
-		let todayDay = myDate.getDate();
-		return `${todayYear}-${todayMonth}-${todayDay}`;
+		let month = myDate.getMonth() + 1;
+		let day = myDate.getDate();
+		return `${year}-${month}-${day}`;
 	}
 	
 	componentDidMount() {
@@ -40,10 +44,10 @@ class Calendar extends Component {
 	
 	getCountsRegisthor = () => {
 		let url = 'https://registhor.da-an.ca/api/v1/offering-counts?key=' + REGISTHOR_API_KEY + '&date_1=' +
-				  this.getISO(this.state.datePickerFrom) + '&date_2=' + this.getISO(this.state.datePickerTo) +
-				  '&course_code=' + this.state.modalCourseCode + '&instructor_name=' + this.state.modalInstructor +
-				  '&exclude_cancelled=' + this.state.modalExcludeCancelled + '&business_line=' + this.state.modalBusinessLine +
-				  '&clients_only=' + this.state.modalClientsOnly;
+				  this.getISO(this.state.inputValues.datePickerFrom) + '&date_2=' + this.getISO(this.state.inputValues.datePickerTo) +
+				  '&course_code=' + this.state.inputValues.modalCourseCode + '&instructor_name=' + this.state.inputValues.modalInstructor +
+				  '&exclude_cancelled=' + this.state.inputValues.modalExcludeCancelled + '&business_line=' + this.state.inputValues.modalBusinessLine +
+				  '&clients_only=' + this.state.inputValues.modalClientsOnly;
 		fetch(url)
 			.then(resp => resp.json())
 			.then((data) => {
@@ -59,7 +63,7 @@ class Calendar extends Component {
 				<h3 className={styles.h3}>Search for offerings by date, instructor, and more.</h3>
 				<CalendarInputs
 					changeInputs={this.changeInputs}
-					currentInputs={this.state}
+					currentInputs={this.state.inputValues}
 				/>
 				<Map cityCounts={this.state.cityCounts} />
 			</>
