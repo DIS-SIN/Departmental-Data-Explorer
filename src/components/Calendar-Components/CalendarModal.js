9495036bcfe41ba1840a6f07151f0218fde0ee42
unknown <38829219+deepklim@users.noreply.github.com>
2019-10-01 16:27:42 -0400

    Rename file as shared across modals

--- src/components/Calendar-Components/CalendarModal.js
+++ src/components/Calendar-Components/CalendarModal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Modal from 'react-modal';
-import styles from './CalendarModal.css';
+import styles from './Modals.css';
 
 function InputBox(props) {
 	return (
