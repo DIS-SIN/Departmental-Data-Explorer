d6615e90a971d655630733867a5cbfe72e992899
unknown <38829219+deepklim@users.noreply.github.com>
2019-10-01 11:41:10 -0400

    Prevent double rendering

--- src/components/Calendar-Components/CalendarInputs.js
+++ src/components/Calendar-Components/CalendarInputs.js
@@ -3,25 +3,33 @@ import CalendarModal from './CalendarModal';
 import DatePicker from './DatePicker';
 import styles from './CalendarInputs.css';
 
-function CalendarInputs(props) {
-	return (
-		<div className={styles.calendarInputs}>
-			<DatePicker
-				label="From:"
-				changeInputs={props.changeInputs}
-				currentInputs={props.currentInputs}
-			/>
-			<CalendarModal
-				changeInputs={props.changeInputs}
-				currentInputs={props.currentInputs}
-			/>
-			<DatePicker
-				label="To:"
-				changeInputs={props.changeInputs}
-				currentInputs={props.currentInputs}
-			/>
-		</div>
-	);
+class CalendarInputs extends Component {
+	shouldComponentUpdate(nextProps, nextState) {
+		// In JS, can't compare values of arrays this way
+		// However, can check if they're the same object in memory
+		return nextProps.currentInputs !== this.props.currentInputs;
+	}
+	
+	render() {
+		return (
+			<div className={styles.calendarInputs}>
+				<DatePicker
+					label="From:"
+					changeInputs={this.props.changeInputs}
+					currentInputs={this.props.currentInputs}
+				/>
+				<CalendarModal
+					changeInputs={this.props.changeInputs}
+					currentInputs={this.props.currentInputs}
+				/>
+				<DatePicker
+					label="To:"
+					changeInputs={this.props.changeInputs}
+					currentInputs={this.props.currentInputs}
+				/>
+			</div>
+		);
+	}
 }
 
 export default CalendarInputs;
