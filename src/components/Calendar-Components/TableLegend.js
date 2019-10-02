68ce5640a14fb8407f9b4fdf63852b55c6a5c4ef
unknown <38829219+deepklim@users.noreply.github.com>
2019-10-02 18:23:29 -0400

    Prevent double rendering

--- src/components/Calendar-Components/TableLegend.js
+++ src/components/Calendar-Components/TableLegend.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './TableLegend.css';
 
 function ResultsCount(props) {
@@ -8,42 +8,47 @@ function ResultsCount(props) {
 	} else {
 		noun = 'Offerings';
 	}
-	
 	return (
 		<h3>{`Results: ${props.count} ${noun}`}</h3>
 	);
 }
 
-function TableLegend(props) {
-	return (
-		<table className={styles.legend}>
-			<thead>
-				<tr>
-					<td colSpan="2">
-						<ResultsCount count={props.count}/>
-					</td>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td className="col-xs-1"><div className={`${styles.colorSwatch} ${styles.swatchDanger}`}></div></td>
-					<td className="col-xs-11">Offerings that have been cancelled</td>
-				</tr>
-				<tr>
-					<td className="col-xs-1"><div className={`${styles.colorSwatch} ${styles.swatchWarning}`}></div></td>
-					<td className="col-xs-11">Offerings taking place in the next 30 days with fewer than 10 confirmed registrations</td>
-				</tr>
-				<tr>
-					<td className="col-xs-1"><div className={`${styles.colorSwatch} ${styles.swatchSuccess}`}></div></td>
-					<td className="col-xs-11">Offerings with more than 10 confirmed registrations and / or that are more than 30 days away</td>
-				</tr>
-				<tr>
-					<td className="col-xs-1"><div className={`${styles.colorSwatch} ${styles.swatchPast}`}></div></td>
-					<td className="col-xs-11">Offerings that have already taken place</td>
-				</tr>
-			</tbody>
-		</table>
-	);
+class TableLegend extends Component {
+	shouldComponentUpdate(nextProps, nextState) {
+		return nextProps.count !== this.props.count;
+	}
+	
+	render() {
+		return (
+			<table className={styles.legend}>
+				<thead>
+					<tr>
+						<td colSpan="2">
+							<ResultsCount count={this.props.count}/>
+						</td>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td className="col-xs-1"><div className={`${styles.colorSwatch} ${styles.swatchDanger}`}></div></td>
+						<td className="col-xs-11">Offerings that have been cancelled</td>
+					</tr>
+					<tr>
+						<td className="col-xs-1"><div className={`${styles.colorSwatch} ${styles.swatchWarning}`}></div></td>
+						<td className="col-xs-11">Offerings taking place in the next 30 days with fewer than 10 confirmed registrations</td>
+					</tr>
+					<tr>
+						<td className="col-xs-1"><div className={`${styles.colorSwatch} ${styles.swatchSuccess}`}></div></td>
+						<td className="col-xs-11">Offerings with more than 10 confirmed registrations and / or that are more than 30 days away</td>
+					</tr>
+					<tr>
+						<td className="col-xs-1"><div className={`${styles.colorSwatch} ${styles.swatchPast}`}></div></td>
+						<td className="col-xs-11">Offerings that have already taken place</td>
+					</tr>
+				</tbody>
+			</table>
+		);
+	}
 }
 
 export default TableLegend;
