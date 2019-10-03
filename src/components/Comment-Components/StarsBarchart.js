10d7beb6c082b3f5d78c15558c2f62d558d088a8
unknown <38829219+deepklim@users.noreply.github.com>
2019-10-03 12:22:18 -0400

    Get counts from Redux and use to set barchart

--- src/components/Comment-Components/StarsBarchart.js
+++ src/components/Comment-Components/StarsBarchart.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import styles from './StarsBarchart.css';
 
 function StarsBarchart(props) {
+	let { 1: oneStar, 2: twoStar, 3: threeStar, 4: fourStar, 5: fiveStar } = props.counts;
+	
+	// Get largest length and use to scale counts to bar widths
+	let upperBound = Math.max(fiveStar, fourStar, threeStar, twoStar, oneStar) * 1.4;
+	
+	// Account for 0 comments
+	upperBound = (upperBound) ? upperBound : 1;
+	
+	// Scale counts to obtain bar width
+	let fiveStarScaled = String(Math.round((fiveStar / upperBound) * 100)) + '%';
+	let fourStarScaled = String(Math.round((fourStar / upperBound) * 100)) + '%';
+	let threeStarScaled = String(Math.round((threeStar / upperBound) * 100)) + '%';
+	let twoStarScaled = String(Math.round((twoStar / upperBound) * 100)) + '%';
+	let oneStarScaled = String(Math.round((oneStar / upperBound) * 100)) + '%';
+	
 	return (
 		<table className={styles.starsBarchart}>
 			<thead>
@@ -19,7 +35,7 @@ function StarsBarchart(props) {
 					<td className="col-xs-2 text-center">Five</td>
 					<td className="col-xs-10">
 						<a href="">
-							<span className={styles.barFill}></span>
+							<span className={styles.barFill} style={{ width: fiveStarScaled }}></span>
 							<span></span>
 						</a>
 					</td>
@@ -28,7 +44,7 @@ function StarsBarchart(props) {
 					<td className="col-xs-2 text-center">Four</td>
 					<td className="col-xs-10">
 						<a href="">
-							<span className={styles.barFill}></span>
+							<span className={styles.barFill} style={{ width: fourStarScaled }}></span>
 							<span></span>
 						</a>
 					</td>
@@ -37,7 +53,7 @@ function StarsBarchart(props) {
 					<td className="col-xs-2 text-center">Three</td>
 					<td className="col-xs-10">
 						<a href="">
-							<span className={styles.barFill}></span>
+							<span className={styles.barFill} style={{ width: threeStarScaled }}></span>
 							<span></span>
 						</a>
 					</td>
@@ -46,7 +62,7 @@ function StarsBarchart(props) {
 					<td className="col-xs-2 text-center">Two</td>
 					<td className="col-xs-10">
 						<a href="">
-							<span className={styles.barFill}></span>
+							<span className={styles.barFill} style={{ width: twoStarScaled }}></span>
 							<span></span>
 						</a>
 					</td>
@@ -55,7 +71,7 @@ function StarsBarchart(props) {
 					<td className="col-xs-2 text-center">One</td>
 					<td className="col-xs-10">
 						<a href="">
-							<span className={styles.barFill}></span>
+							<span className={styles.barFill} style={{ width: oneStarScaled }}></span>
 							<span></span>
 						</a>
 					</td>
@@ -65,4 +81,10 @@ function StarsBarchart(props) {
 	);
 }
 
-export default StarsBarchart;
+const mapStateToProps = (state) => {
+	return { counts: state.commentReducer.counts };
+}
+
+const mapActionsToProps = {};
+
+export default connect(mapStateToProps, mapActionsToProps)(StarsBarchart);
