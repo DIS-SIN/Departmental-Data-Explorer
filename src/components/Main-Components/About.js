0d6c790a40127b3bbda31b74f27b7fe7073ed124
unknown <38829219+deepklim@users.noreply.github.com>
2019-10-11 17:21:51 -0400

    Style and add text to About tab

--- src/components/Main-Components/About.js
+++ src/components/Main-Components/About.js
@@ -3,7 +3,24 @@ import styles from './About.css';
 
 function About() {
 	return (
-		<h1>We're a big deal</h1>
+		<div className={styles.about}>
+			<h3>This app aims to give you easy, instant access to your department's School data.</h3>
+			<p>Features:</p>
+			<ul>
+				<li>View charts showing learning KPIs</li>
+				<li>Read all comments submitted by your learners since 2015</li>
+				<li>View maps of where in Canada your learners are taking School courses</li>
+				<li>Download your department's raw data for both registrations and comments</li>
+				<li>Find upcoming School courses via an intereactive calendar</li>
+				<li>Indicate which School courses your department considers mandatory</li>
+			</ul>
+			<p>
+				If you encounter any bugs or would like features added, please contact <a href="mailto:sam.louden@canada.ca">Sam Louden</a>.
+			</p>
+			<h4>
+				Data in all tabs are updated daily at 3:00am EST.
+			</h4>
+		</div>
 	);
 }
 
