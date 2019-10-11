68a5cdf88c3fcad4fbc0956aaf8655f2275b97a3
unknown <38829219+deepklim@users.noreply.github.com>
2019-10-11 16:35:50 -0400

    Add routes for Home and About

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './static/App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
+import AboutParent from './components/Main-Components/AboutParent';
 import CalendarParent from './components/Calendar-Components/CalendarParent';
 import CommentsParent from './components/Comment-Components/CommentsParent';
+import HomeParent from './components/Main-Components/HomeParent';
 import Splash from './components/Main-Components/Splash';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Route exact path="/" component={Splash} />
+			<Route exact path="/about" component={AboutParent} />
 			<Route exact path="/calendar" component={CalendarParent} />
 			<Route exact path="/comments" component={CommentsParent} />
+			<Route exact path="/en" component={HomeParent} />
 		</BrowserRouter>
 	);
 }
