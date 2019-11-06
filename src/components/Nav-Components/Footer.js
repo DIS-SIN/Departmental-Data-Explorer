524ba2b54ca4fb72afefa5ea11c7c8ad512a707b
unknown <38829219+deepklim@users.noreply.github.com>
2019-11-06 14:55:48 -0500

    Update 'Top of Page' button now that using HashRouter

--- src/components/Nav-Components/Footer.js
+++ src/components/Nav-Components/Footer.js
@@ -3,12 +3,23 @@ import Container from '@material-ui/core/Container';
 import Canada from '../../static/FIPs/canada_colour.svg';
 import styles from './Footer.css';
 
+function TopOfPage() {
+	const onClick = (e) => {
+		e.preventDefault();
+		window.scrollTo(0, 0);
+	}
+	
+	return (
+		<a className={styles.topOfPage} onClick={onClick}>Top of Page</a>
+	);
+}
+
 function Footer() {
 	return (
 		<footer className={styles.footer}>
 			<Container className={styles.footerContainer}>
 				<div className={styles.footerContents}>
-					<a href="#top">Top of Page</a>
+					<TopOfPage />
 					<img src={Canada} alt="Canada Logo" />
 				</div>
 			</Container>
